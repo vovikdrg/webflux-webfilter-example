432ce4e37087aa8f2cd1f8b18b799b90223501d4
Volodymyr BIlyachat <vovikdrg@gmail.com>
2021-05-09 21:19:38 +1000

    Web filter

---
+++ src/test/java/com/vob/webflux/webfilter/WebfilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vob.webflux.webfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfilterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
